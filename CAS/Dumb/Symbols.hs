1eb896dc068f36377372c98b70d9811801fc60b2
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2017-07-01 16:18:51 +0200

    Avoid unnecessary parens.

--- CAS/Dumb/Symbols.hs
+++ CAS/Dumb/Symbols.hs
@@ -35,17 +35,40 @@ type SEncapsulation = Encapsulation String
 parenthesise :: SEncapsulation
 parenthesise = Encapsulation "(" ")"
       
-symbolInfix :: Infix s² -> s¹
+symbolInfix :: Infix s² -- ^ The operator we want to describe
+            -> s¹       -- ^ Parenthesization action, in case the operands bind too weakly
   -> CAS' γ (Infix s²) s¹ s⁰ -> CAS' γ (Infix s²) s¹ s⁰ -> CAS' γ (Infix s²) s¹ s⁰
-symbolInfix infx encaps a b = Operator infx (Function encaps a) (Function encaps b)
+symbolInfix infx@(Infix (Hs.Fixity fxty fxdir) _) encaps a b
+               = Operator infx (secureL a) (secureR b)
+ where secureL x@(Operator (Infix (Hs.Fixity lfxty _) _) _ _)
+        | lfxty < fxty  = Function encaps x
+       secureL x@(Operator (Infix (Hs.Fixity lfxty Hs.InfixL) _) _ _)
+        | Hs.InfixL <- fxdir
+        , lfxty==fxty   = x
+       secureL x@(Operator (Infix (Hs.Fixity lfxty _) _) _ _)
+        | lfxty==fxty  = Function encaps x
+       secureL x  = x
+       secureR x@(Operator (Infix (Hs.Fixity lfxty _) _) _ _)
+        | lfxty > fxty  = x
+       secureR x@(Operator (Infix (Hs.Fixity lfxty Hs.InfixR) _) _ _)
+        | Hs.InfixR <- fxdir
+        , lfxty==fxty   = x
+       secureR x@(Operator (Infix (Hs.Fixity lfxty _) _) _ _)
+        | lfxty==fxty  = Function encaps x
+       secureR x  = x
 
 symbolFunction :: Monoid s¹ => s¹ -> Encapsulation s¹
   -> CAS' γ (Infix s²) (Encapsulation s¹) s⁰
   -> CAS' γ (Infix s²) (Encapsulation s¹) s⁰
-symbolFunction f (Encapsulation l r) a = Function (Encapsulation (f<>l) r) a
+symbolFunction f (Encapsulation l r) a@(Symbol _)
+    = Function (Encapsulation f mempty) a
+symbolFunction f (Encapsulation l r) a
+    = Function (Encapsulation (f<>l) r) a
 
 instance Num (CAS' γ InfixSymbol SEncapsulation Symbol) where
-  fromInteger = Symbol . NatSymbol
+  fromInteger n
+   | n<0        = negate . fromInteger $ -n
+   | otherwise  = Symbol $ NatSymbol n
   (+) = symbolInfix (Infix (Hs.Fixity 6 Hs.InfixL) "+") parenthesise
   (*) = symbolInfix (Infix (Hs.Fixity 7 Hs.InfixL) "*") parenthesise
   (-) = symbolInfix (Infix (Hs.Fixity 6 Hs.InfixL) "-") parenthesise
