cebd0bc9213397245f6bc88c3d3596b18883524f
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2017-07-01 17:59:55 +0200

    Add support for constructing and showing gaps.

--- CAS/Dumb/Symbols/ASCII.hs
+++ CAS/Dumb/Symbols/ASCII.hs
@@ -20,6 +20,18 @@ a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z :: CAS' γ s² s¹ Symbol
 [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
     = Symbol . StringSymbol . pure <$> ['a'..'z']
 
+_a,_b,_c,_d,_e,_f,_g,_h,_i,_j,_k,_l,_m,_n,_o,_p,_q,_r,_s,_t,_u,_v,_w,_x,_y,_z
+    :: CAS' GapId s² s¹ s⁰
+[_a,_b,_c,_d,_e,_f,_g,_h,_i,_j,_k,_l,_m,_n,_o,_p,_q,_r,_s,_t,_u,_v,_w,_x,_y,_z]
+    = Gap . fromEnum <$> ['a'..'z']
+
 
 instance Show (CAS InfixSymbol SEncapsulation Symbol) where
   showsPrec = showsPrecASCIISymbol
+instance Show (CAS' GapId InfixSymbol SEncapsulation Symbol) where
+  showsPrec p = showsPrecASCIISymbol p . purgeGaps
+   where purgeGaps (Symbol s) = Symbol s
+         purgeGaps (Function f e) = Function f $ purgeGaps e
+         purgeGaps (Operator o x y) = Operator o (purgeGaps x) (purgeGaps y)
+         purgeGaps (Gap gid) = Symbol (StringSymbol ['_',toEnum gid])
+                                          :: (CAS InfixSymbol SEncapsulation Symbol)
