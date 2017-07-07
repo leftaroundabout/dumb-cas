c0162e3cd50ca5b3dfe4e3c26774b8a1e8b704f5
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2017-07-07 12:51:01 +0200

    Remove most of the symbol exports.

--- CAS/Dumb/Symbols/ASCII.hs
+++ CAS/Dumb/Symbols/ASCII.hs
@@ -21,17 +21,6 @@
 
 module CAS.Dumb.Symbols.ASCII (
           module CAS.Dumb.Symbols
-        , Symbol
-        -- * “Constant variable” symbols
-        -- ** Lowercase letters
-        , a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z
-        -- ** Uppercase letters
-        -- $uppercaseCaveat
-#if __GLASGOW_HASKELL__ > 802
-        , pattern A, pattern B, pattern C, pattern D, pattern E, pattern F, pattern G, pattern H, pattern I, pattern J, pattern K, pattern L, pattern M, pattern N, pattern O, pattern P, pattern Q, pattern R, pattern S, pattern T, pattern U, pattern V, pattern W, pattern X, pattern Y, pattern Z
-#endif
-        -- * Pattern-matching variable symbols
-        , _a,_b,_c,_d,_e,_f,_g,_h,_i,_j,_k,_l,_m,_n,_o,_p,_q,_r,_s,_t,_u,_v,_w,_x,_y,_z
         ) where
 
 import CAS.Dumb.Tree
@@ -42,15 +31,6 @@ data ASCII
 type Symbol = SymbolD ASCII
 type Expression' γ s² s¹ = CAS' γ s² s¹ Symbol
 
-a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z :: CAS' γ s² s¹ Symbol
-[a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
-    = Symbol . StringSymbol . pure <$> ['a'..'z']
-
-_a,_b,_c,_d,_e,_f,_g,_h,_i,_j,_k,_l,_m,_n,_o,_p,_q,_r,_s,_t,_u,_v,_w,_x,_y,_z
-    :: CAS' GapId s² s¹ s⁰
-[_a,_b,_c,_d,_e,_f,_g,_h,_i,_j,_k,_l,_m,_n,_o,_p,_q,_r,_s,_t,_u,_v,_w,_x,_y,_z]
-    = Gap . fromEnum <$> ['a'..'z']
-
 -- $uppercaseCaveat
 -- These are only available in GHC>8.2. The ability to use uppercase letters as variables
 -- hinges on a hack using GHC's still recent
@@ -59,12 +39,3 @@ _a,_b,_c,_d,_e,_f,_g,_h,_i,_j,_k,_l,_m,_n,_o,_p,_q,_r,_s,_t,_u,_v,_w,_x,_y,_z
 mkUppercaseSymbols ''Expression' ['A'..'Z']
 #endif
 
-instance Show (CAS InfixSymbol SEncapsulation Symbol) where
-  showsPrec = showsPrecASCIISymbol
-instance Show (CAS' GapId InfixSymbol SEncapsulation Symbol) where
-  showsPrec p = showsPrecASCIISymbol p . purgeGaps
-   where purgeGaps (Symbol s) = Symbol s
-         purgeGaps (Function f e) = Function f $ purgeGaps e
-         purgeGaps (Operator o x y) = Operator o (purgeGaps x) (purgeGaps y)
-         purgeGaps (Gap gid) = Symbol (StringSymbol ['_',toEnum gid])
-                                          :: (CAS InfixSymbol SEncapsulation Symbol)
